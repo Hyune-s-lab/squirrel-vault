6a306aeb2c13adcda7d8a3fdfac16ececff8e8cb
Hyune-c <choi8608@gmail.com>
2024-10-14 01:50:04 +0900

    [과금 정보 구현] acorn 모델 설계

--- core/core-domain/src/main/kotlin/dev/hyunec/squirrelvault/coredomain/model/Acorn.kt
+++ core/core-domain/src/main/kotlin/dev/hyunec/squirrelvault/coredomain/model/Acorn.kt
@@ -1,8 +1,52 @@
 package dev.hyunec.squirrelvault.coredomain.model
 
-import java.time.Instant
-
+/**
+ * 과금 정보 (도토리)
+ *
+ * @property type 과금 구분
+ * @property subType 과금 세부 구분. NONE 은 해당 사항 없음을 의미
+ */
 data class Acorn(
-    val type: String,
-    val collectedAt: Instant
-)
+    val source: Source,
+
+    val type: Type,
+    val subType: SubType,
+
+    val memo: String
+) {
+    enum class Type {
+        MODEL_CALL,
+        DOCUMENT,
+        NOTIFICATION,
+    }
+
+    enum class SubType {
+        // MODEL_CALL
+        CHAT,
+        FUNCTION_CALL,
+        TEXT_GENERATION,
+        IMAGE_GENERATION,
+        TRANSLATION,
+        SUMMARIZATION,
+        QUESTION_ANSWERING,
+        SENTIMENT_ANALYSIS,
+        DATA_EXTRACTION,
+        CUSTOM_MODEL,
+
+        // DOCUMENT
+        SUMMARY,
+        PDF_CONVERSION,
+        TEXT_EXTRACTION,
+        CLASSIFICATION,
+        OCR_PROCESSING,
+
+        // NOTIFICATION
+        KAKAOTALK,
+        EMAIL,
+        SLACK,
+        SMS,
+
+        // NOT APPLICABLE
+        NONE
+    }
+}
