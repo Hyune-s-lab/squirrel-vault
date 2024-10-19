3dd59aae16007df4c0f8badb7b9306f671ffcb98
Hyune-c <choi8608@gmail.com>
2024-10-20 00:31:03 +0900

    [demo 구현] statistics summary 구현

--- core/acorn-collector-api/src/main/kotlin/dev/hyunec/squirrelvault/acorncollectorapi/controller/StatisticsController.kt
+++ core/acorn-collector-api/src/main/kotlin/dev/hyunec/squirrelvault/acorncollectorapi/controller/StatisticsController.kt
@@ -1,16 +1,32 @@
 package dev.hyunec.squirrelvault.acorncollectorapi.controller
 
 import dev.hyunec.squirrelvault.acorncollectorapi.facade.StatisticsFacade
-import dev.hyunec.squirrelvault.coredomain.model.Acorn
 import org.springframework.web.bind.annotation.GetMapping
+import org.springframework.web.bind.annotation.RequestParam
 import org.springframework.web.bind.annotation.RestController
+import java.time.LocalDate
+import java.time.LocalTime
+import java.time.ZoneId
 
 @RestController
 class StatisticsController(
     private val statisticsFacade: StatisticsFacade
 ) {
-    @GetMapping("/api/v1/statistics/")
-    fun readAll(): List<Acorn> {
-        return statisticsFacade.readAll()
+    @GetMapping("/api/v1/statistics/summary")
+    fun summary(
+        @RequestParam startDate: LocalDate,
+        @RequestParam endDate: LocalDate,
+        @RequestParam requesterIds: List<String>
+    ): Response {
+        val startInstant = startDate.atStartOfDay(ZoneId.of("Asia/Seoul")).toInstant()
+        val endInstant = endDate.atTime(LocalTime.MAX).atZone(ZoneId.of("Asia/Seoul")).toInstant()
+
+        return Response(
+            data = statisticsFacade.summary(startInstant, endInstant, requesterIds)
+        )
     }
+
+    data class Response(
+        val data: Map<String, String>
+    )
 }
--- core/acorn-collector-api/src/main/kotlin/dev/hyunec/squirrelvault/acorncollectorapi/facade/StatisticsFacade.kt
+++ core/acorn-collector-api/src/main/kotlin/dev/hyunec/squirrelvault/acorncollectorapi/facade/StatisticsFacade.kt
@@ -1,9 +1,9 @@
 package dev.hyunec.squirrelvault.acorncollectorapi.facade
 
-import dev.hyunec.squirrelvault.coredomain.model.Acorn
 import dev.hyunec.squirrelvault.coredomain.repository.AcornRepository
 import io.github.oshai.kotlinlogging.KotlinLogging
 import org.springframework.stereotype.Service
+import java.time.Instant
 
 @Service
 class StatisticsFacade(
@@ -11,7 +11,15 @@ class StatisticsFacade(
 ) {
     private val log = KotlinLogging.logger {}
 
-    fun readAll(): List<Acorn> {
+    fun summary(
+        startInstant: Instant,
+        endInstant: Instant,
+        requesterIds: List<String>
+    ): Map<String, String> {
         return acornRepository.findAll()
+            .filter { it.source.task.requesterId in requesterIds }
+            .filter { it.source.task.completedAt in startInstant..endInstant }
+            .groupBy { "${it.type}.${it.subType}" }
+            .mapValues { it.value.size.toString() }
     }
 }
